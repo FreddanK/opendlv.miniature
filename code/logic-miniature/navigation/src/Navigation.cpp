43f58b4a71a9912b74d1f7335a44537ac53e6316
Fredrik Kjellberg <fkakg@hotmail.com>
2017-04-11 11:47:41 +0200

    Add very basic exploration and obstacle avoidance behaviour.

--- code/logic-miniature/navigation/src/Navigation.cpp
+++ code/logic-miniature/navigation/src/Navigation.cpp
@@ -103,54 +103,136 @@ odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode Navigation::body()
 
     //// Example below.
     
-    std::cout << "TODO: Remove example." << std::endl;
+    // Constant definintions
+    float const MAX_VOLTAGE = 1.8;
+    float const IR_MAX_DISTANCE = 3;
+    float const SONIC_MAX_DISTANCE = 20;
+
+    uint32_t const PWM_NEUTRAL = 1500000;
+    uint32_t const PWM_MAX = 2000000;
+    uint32_t const PWM_MIN = 1000000;
+    uint32_t const PWM_STEP = 100000;
+
+
+    uint32_t leftOutputPWM = PWM_NEUTRAL;
+    uint32_t rightOutputPWM = PWM_NEUTRAL;
 
     // Print some data collected from the 'nextContainer' method below.
-    float voltageReadingPin0 = m_analogReadings[0];
-    std::cout << "Reading from analog pin 0: " << voltageReadingPin0 << std::endl;
+    //std::cout << "Reading from analog pin 0: " << voltageReadingPin0 << std::endl;
+    
+    float voltageMiddleIR = m_analogReadings[0];
+    float voltageRightSonic = m_analogReadings[1];
+    float voltageLeftSonic = m_analogReadings[2];
+
+    float distanceMiddleIR = (voltageMiddleIR/MAX_VOLTAGE)*IR_MAX_DISTANCE;
+    float distanceRightSonic = (voltageRightSonic/MAX_VOLTAGE)*SONIC_MAX_DISTANCE;
+    float distanceLeftSonic = (voltageLeftSonic/MAX_VOLTAGE)*SONIC_MAX_DISTANCE;
+
+    std::cout << "Distance middle infrared: " << distanceMiddleIR << std::endl;
+    std::cout << "Distance right sonic: " << distanceRightSonic << std::endl;
+    std::cout << "Distance left sonic: " << distanceLeftSonic << std::endl;
+
+    float d = 2.5;
+    if(distanceMiddleIR > d)
+    {
+      if(distanceRightSonic > 10 && distanceLeftSonic > 10) 
+      {
+        leftOutputPWM = PWM_NEUTRAL + PWM_STEP;
+        rightOutputPWM = PWM_NEUTRAL + PWM_STEP;
+      }
+      else if(abs(distanceRightSonic - distanceLeftSonic) < 1)  
+      {
+        leftOutputPWM = PWM_NEUTRAL + PWM_STEP;
+        rightOutputPWM = PWM_NEUTRAL + PWM_STEP;
+      }
+      else if(distanceRightSonic > distanceLeftSonic)
+      {
+        int diff = distanceRightSonic - distanceLeftSonic;
+        leftOutputPWM = PWM_NEUTRAL + diff*PWM_STEP;
+        rightOutputPWM = PWM_NEUTRAL + PWM_STEP;
+      }
+      else if(distanceRightSonic < distanceLeftSonic)
+      {
+        int diff = distanceLeftSonic - distanceRightSonic;
+        leftOutputPWM = PWM_NEUTRAL + PWM_STEP;
+        rightOutputPWM = PWM_NEUTRAL + diff*PWM_STEP;
+      }
+    }
+    else
+    {
+      if(abs(distanceRightSonic - distanceLeftSonic) > 1 && distanceRightSonic > distanceLeftSonic)
+      {
+        leftOutputPWM = PWM_NEUTRAL + PWM_STEP;
+        rightOutputPWM = PWM_NEUTRAL - PWM_STEP;
+      }
+      else
+      {
+        leftOutputPWM = PWM_NEUTRAL - PWM_STEP;
+        rightOutputPWM = PWM_NEUTRAL + PWM_STEP;
+      }
+    }
+
 
     // Loop through all General Purpose IO (GPIO) pins and randomize their 
     // state. The state is then sent as a message to the module interfacing
     // to the actual hardware.
-    for (auto pin : m_gpioOutputPins) {
-      bool value = static_cast<bool>(std::rand() % 2);
+    //for (auto pin : m_gpioOutputPins) {
+    //  bool value = static_cast<bool>(std::rand() % 2);
 
-      opendlv::proxy::ToggleRequest::ToggleState state;
-      if (value) {
-        state = opendlv::proxy::ToggleRequest::On;
-      } else {
-        state = opendlv::proxy::ToggleRequest::Off;
-      }
+    //  opendlv::proxy::ToggleRequest::ToggleState state;
+    //  if (value) {
+    //    state = opendlv::proxy::ToggleRequest::On;
+    //  } else {
+    //    state = opendlv::proxy::ToggleRequest::Off;
+    //  }
 
-      opendlv::proxy::ToggleRequest request(pin, state);
-      
-      odcore::data::Container c(request);
-      getConference().send(c);
-      
-      std::cout << "[" << getName() << "] Sending ToggleRequest: " 
-          << request.toString() << std::endl;
-    }
+    //  opendlv::proxy::ToggleRequest request(pin, state);
+    //  
+    //  odcore::data::Container c(request);
+    //  getConference().send(c);
+    //  
+    //  std::cout << "[" << getName() << "] Sending ToggleRequest: " 
+    //      << request.toString() << std::endl;
+    //}
+
+    if(leftOutputPWM > PWM_MAX) leftOutputPWM = PWM_MAX;
+    else if(leftOutputPWM < PWM_MIN) leftOutputPWM = PWM_MIN;
+
+    if(rightOutputPWM > PWM_MAX) rightOutputPWM = PWM_MAX;
+    else if(rightOutputPWM < PWM_MIN) rightOutputPWM = PWM_MIN;
+
+    uint16_t leftWheelPin = 0;
+    uint16_t rightWheelPin = 1;
+
+    opendlv::proxy::PwmRequest requestLeft(leftWheelPin,leftOutputPWM);
+    opendlv::proxy::PwmRequest requestRight(rightWheelPin,rightOutputPWM);
+
+    odcore::data::Container cL(requestLeft);
+    odcore::data::Container cR(requestRight);
+
+    getConference().send(cL);
+    getConference().send(cR);
 
     // Loop through all Pulse Width Modulation (PWM) pins and randomize their 
     // value. The value is then sent as a message to the module interfacing
     // to the actual hardware.
-    for (auto pin : m_pwmOutputPins) {
-      int32_t rand = (std::rand() % 11) - 5 ;
-      uint32_t value = 1500000 + rand * 100000;
-      
-      opendlv::proxy::PwmRequest request(pin, value);
-      
-      odcore::data::Container c(request);
-      getConference().send(c);
-      
-      std::cout << "[" << getName() << "] Sending PwmRequest: " 
-          << request.toString() << std::endl;
-    }
+    //for (auto pin : m_pwmOutputPins) {
+    //  int32_t rand = (std::rand() % 11) - 5 ;
+    //  uint32_t value = 1500000 + rand * 100000;
+    //  
+    //  opendlv::proxy::PwmRequest request(pin, value);
+    //  
+    //  odcore::data::Container c(request);
+    //  getConference().send(c);
+    //  
+    //  std::cout << "[" << getName() << "] Sending PwmRequest: " 
+    //      << request.toString() << std::endl;
+    //}
 
     ///// Example above.
 
     ///// TODO: Add proper behaviours.
-    std::cout << "TODO: Add proper behaviour." << std::endl;
+    //std::cout << "TODO: Add proper behaviour." << std::endl;
   }
   return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
 }
